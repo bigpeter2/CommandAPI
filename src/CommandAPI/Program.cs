80dbc783bb701a1f027c7bec2dccb0fc2c3e3980
bigpeter2 <bigpeter2@gmail.com>
2025-02-12 16:46:41 +0800

    initial commit

---
+++ src/CommandAPI/Program.cs
@@ -0,0 +1,30 @@
+// var builder = WebApplication.CreateBuilder(args);
+// var app = builder.Build();
+
+// app.MapGet("/", () => "Hello World!");
+
+// app.Run();
+
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
